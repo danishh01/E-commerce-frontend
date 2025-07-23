0f623e37a4f84caa75d0852990bd8bf56e099153
Danish Surah <mr.surah12345@gmail.com>
2025-07-23 17:07:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Products from "./components/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AddProduct from "./components/AddProduct";
+import ProductDetail from "./components/ProductDetail";
+import AdminProduct from "./components/AdminProduct";
+import EditProduct from "./components/EditProduct";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+import Address from "./components/Address";
+import Checkout from "./components/Checkout";
+import SearchResult from "./components/SearchResult";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Profile from "./components/Profile";
+import Order_Confirmation from "./components/Order_Confirmation";
+import AllUsers from "./components/AllUsers";
+const App = () => {
+  return (
+    <>
+      
+        <Navbar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/search/:term" element={<SearchResult />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/address" element={<Address />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orderconfirmation" element={<Order_Confirmation />} />
+          <Route path="/admin" element={<AdminProduct />} />
+          <Route path="/admin/add" element={<AddProduct />} />
+          <Route path="/admin/edit/:id" element={<EditProduct />} />
+          <Route path="/admin/allorder" element={<Profile />} />
+          <Route path="/admin/allusers" element={<AllUsers />} />
+        </Routes>
+      
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,193 @@
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import AppContext from "../context/AppCotext";
+
+const Navbar = () => {
+  const {
+    logout,
+    isAuthenticated,
+    cart,
+    filteredData,
+    setfilteredData,
+    products,
+  } = useContext(AppContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      navigate(`/search/${searchTerm.toLowerCase()}`); //
+      setSearchTerm("");
+    }
+  };
+
+  const filterByCategory = (cat) => {
+    setfilteredData(
+      products.filter(
+        (data) => data.category.toLowerCase() === cat.toLowerCase()
+      )
+    );
+  };
+
+  const filterByPrice = (price) => {
+    setfilteredData(products.filter((data) => data.price >= Number(price))); // ✅ convert price to number
+  };
+
+  return (
+    <>
+      <div className="nav sticky-top">
+        <div className="nav_bar">
+          {location.pathname.includes("/admin") ? (
+            <Link
+              to={"/admin"}
+              className="left"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Admin Panel
+            </Link>
+          ) : (
+            <Link
+              to={"/"}
+              className="left"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              GADGETS STORE
+            </Link>
+          )}
+
+          <form onSubmit={handleSubmit} className="search_bar">
+            <span className="material-symbols-outlined">search</span>
+            <input
+              type="text"
+              value={searchTerm}
+              placeholder="Search by category..."
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </form>
+
+          <div className="right">
+            {!location.pathname.includes("/admin") && (
+              <>
+                {!isAuthenticated ? (
+                  <>
+                    <Link to={"/login"} className="btn btn-info mx-2">
+                      Login
+                    </Link>
+                    <Link to={"/register"} className="btn btn-secondary mx-2">
+                      SignUp
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link
+                      to={"/cart"}
+                      className="btn btn-primary position-relative mx-3"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      <span className="material-symbols-outlined">
+                        shopping_cart
+                      </span>
+                      {cart?.items?.length > 0 && (
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                          {cart.items.length}
+                          <span className="visually-hidden">items</span>
+                        </span>
+                      )}
+                    </Link>
+                    <Link
+                      to={"/profile"}
+                      className="btn btn-warning mx-2"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Profile
+                    </Link>
+                    <button
+                      className="btn btn-danger mx-3"
+                      style={{ fontWeight: "bold" }}
+                      onClick={() => {
+                        logout();
+                        navigate("/");
+                      }}
+                    >
+                      LogOut
+                    </button>
+                    <Link
+                      to={"/admin"}
+                      className="btn btn-info mx-2"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Admin
+                    </Link>
+                  </>
+                )}
+              </>
+            )}
+
+            {location.pathname.includes("/admin") && (
+              <>
+                <Link to={"/"} className="btn btn-info mx-2">
+                  Go To Home
+                </Link>
+                <Link to={"/admin/add"} className="btn btn-warning mx-2">
+                  Add Product
+                </Link>
+                <Link to={"/admin/allorder"} className="btn btn-secondary mx-2">
+                  All Order's
+                </Link>
+                <Link to={"/admin/allusers"} className="btn btn-info mx-2">
+                  All Users
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+
+        {location.pathname === "/" && (
+          <div className="sub_bar">
+            <div className="items" onClick={() => setfilteredData(products)}>
+              No Filter
+            </div>
+            <div className="items" onClick={() => filterByCategory("mobiles")}>
+              Mobiles
+            </div>
+            <div className="items" onClick={() => filterByCategory("laptops")}>
+              Laptops
+            </div>
+            <div className="items" onClick={() => filterByCategory("tablets")}>
+              Tablets
+            </div>
+            <div className="items" onClick={() => filterByCategory("cameras")}>
+              Camera's
+            </div>
+            <div
+              className="items"
+              onClick={() => filterByCategory("headphones")}
+            >
+              HeadPhones
+            </div>
+            <div className="items" onClick={() => filterByPrice(19999)}>
+              19999
+            </div>
+            <div className="items" onClick={() => filterByPrice(39999)}>
+              39999
+            </div>
+            <div className="items" onClick={() => filterByPrice(49999)}>
+              49999
+            </div>
+            <div className="items" onClick={() => filterByPrice(65999)}>
+              65999
+            </div>
+            <div className="items" onClick={() => filterByPrice(89999)}>
+              89999
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
